f96374cdb698646dff7e559089d04f2368144540
jayrajvmu <jayarajvmu@gmail.com>
2023-05-08 00:47:56 +0530

    Initial Project

---
+++ application/views/components/head.php
@@ -0,0 +1,11 @@
+<head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <meta name="description" content="Askbootstrap">
+      <meta name="author" content="Askbootstrap">
+      <title>:: Creative Agency ::</title>
+      <link rel="stylesheet"href="<?php echo base_url(); ?>assets/css/vendor/odometer-theme-default.css">
+
+<!-- Site Stylesheet -->
+<link rel="stylesheet"href="<?php echo base_url(); ?>assets/css/app.css">
+   </head>
---
+++ application/views/components/js.php
@@ -0,0 +1,22 @@
+
+
+
+          <!-- Jquery Js -->
+    <script src="<?php echo base_url(); ?>assets/js/vendor/jquery-3.6.0.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/vendor/bootstrap.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/vendor/isotope.pkgd.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/vendor/imagesloaded.pkgd.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/vendor/odometer.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/vendor/jquery-appear.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/vendor/slick.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/vendor/sal.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/vendor/jquery.magnific-popup.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/vendor/js.cookie.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/vendor/jquery.style.switcher.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/vendor/jquery.countdown.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/vendor/tilt.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/vendor/green-audio-player.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/vendor/jquery.nav.js"></script>
+
+    <!-- Site Scripts -->
+    <script src="<?php echo base_url(); ?>assets/js/app.js"></script>
